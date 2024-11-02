296c552084d86bdc2bd06db804e1353c68520e21
Aya-Bouchama <bouchamaaya34@gmail.com>
2024-11-02 13:07:13 +0100

    first commit

--- ProjetGL/src/ac/ma/ensmr/hello.java
+++ ProjetGL/src/ac/ma/ensmr/hello.java
@@ -1,10 +1,11 @@
-package ma.ac.ensmr;
+package ac.ma.ensmr;
 
-public class Hello {
+public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, world!");
+
 	}
 
 }
